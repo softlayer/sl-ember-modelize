f29d51bbeb21fd6122c9592ab2d48031b1cf53f8
Matthew Marcum <mmarcum@softlayer.com>
2014-10-03 15:51:46 -0500

    updated to .47, added unit tests and docs

---
+++ tests/unit/mixins/modelize-test.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';                                                                                                                         
+import SlModelize from 'sl-modelize/mixins/modelize';        
+
+var subject;
+
+module("Unit - mixins/modelize", {
+    setup: function() {
+        subject = Ember.Object.createWithMixins( SlModelize );
+    }
+});
+                                                                                    
+test('it mixes in', function() {                                                       
+   ok( SlModelize.detect( subject ), "Modelize function gets mixed in");
+});   
