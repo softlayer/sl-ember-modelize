526e2282c31c7c3480f34dce75381c8dd975b940
Jeremy Brown <jebrown@softlayer.com>
2014-12-18 15:00:42 -0600

    Closes softlayer/sl-ember-modelize#39

--- index.js
+++ index.js
@@ -2,21 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'sl-ember-modelize',
-
-  contentFor: function( type, config ) {
-    var content;
-
-    switch ( type ) {
-        case 'keywords':
-            content = require('./package.json')['keywords'].join( ', ' ) + ', ember, ember cli';
-            break;
-
-        case 'description':
-            content = require('./package.json')['description'];
-            break;
-    }
-
-    return content;
-  }
+  name: 'sl-ember-modelize'
 };
